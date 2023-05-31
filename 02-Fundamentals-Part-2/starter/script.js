403649127d2b12181fc553f94db64cb5169e4773
sophie <52949826+sophunky@users.noreply.github.com>
2023-05-31 16:29:23 -0400

    functions lesson complete

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 /**
- //ACTIVATING STRICT MODE
+ // ACTIVATING STRICT MODE
  strict mode forbids us from doing certain things
  and gives extra error reporting in console
   
@@ -8,5 +8,25 @@
  if (passTest) hasDriverLicense = true; // error.. example of what strict mode does in the console
  */
 
+ 'use strict'; // activates script mode.. has to be first code in file
 
-'use strict'; // activates script mode.. has to be first code in file
+// FUNCTIONS
+function logger () {
+    // inside of function is called a function buddy
+    console.log("My name is Sophie");
+    // use function anytime you need to reuse code
+}
+// calling/running/invoking function are interchangeable words
+
+// function w/ parameters
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+    return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
