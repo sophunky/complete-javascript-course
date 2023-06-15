7941b083d8663a69518dbea002cd5114051cc294
sophie <52949826+sophunky@users.noreply.github.com>
2023-06-15 17:15:45 -0400

    FUNCTION DECLARATIONS VS. EXPRESSIONS

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -7,6 +7,7 @@
  const passTest = true;
  if (passTest) hasDriverLicense = true; // error.. example of what strict mode does in the console
 
+'use strict'; // activates script mode.. has to be first code in file
 
 // FUNCTIONS
 function logger () {
@@ -28,6 +29,25 @@ console.log(appleJuice);
 
 const appleOrangeJuice = fruitProcessor(2, 4);
 console.log(appleOrangeJuice);
+
+// FUNCTION DECLARATIONS VS. EXPRESSIONS
+
+// function declaration
+function calcAge1(birthYear) {
+    return 2023 - birthYear;
+}
+const age1 = calcAge1(1991);
+
+// function expression
+const calcAge2 = function(birthYear){ // anonymous function b/c it's nameless
+    return 2023 - birthYear;
+}
+const age2 = calcAge2(1991);
+
+console.log(age1, age2);
+
+// reminder: expressions produce values
+// in JS, functions are values and so they can be stored in variables
  */
 
- 'use strict'; // activates script mode.. has to be first code in file
+
